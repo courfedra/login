2d6fa070310af8d2ee0fd7c2a6f752558e807f2a
courfedra <franandres93@gmail.com>
2023-01-01 17:16:26 -0300

    styled to button login

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import './App.css'
-import { LoginButton, LogoutButton,Profile } from './components/Login'
+import { LoginButton, Profile } from './components/Login'
 import { useAuth0 } from '@auth0/auth0-react'
+import styled from 'styled-components'
+
 function App() {
   const {isAuthenticated} = useAuth0()
+  const Card = styled.div`
+    width:50vw;
+    height:50vh;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    background-color:#ababab33;
+    border-radius:15px;
+`
+
 
   return (
-    <>
+    <Card>
       {isAuthenticated
-      ?<>
-        <Profile/>
-        <LogoutButton/>
-        </>
+      ?<Profile/>
       :<LoginButton/>
       }
-    </>
+    </Card>
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,17 +3,31 @@ import { useAuth0 } from "@auth0/auth0-react";
 import styled from 'styled-components'
 
 const LoginBtn = styled.button`
-    background-color:red
+    background-color:#101010;
+    color:#f5f5f5;
+    transition: all .15s ease-in-out;
+    &:hover{
+        background:#101010aa;
+        border: 1.5px solid #101010;
+    }
+`
+
+const LoginImg = styled.img`
+    padding: 5px;
+    margin: 5px;
 `
 
 export const LoginButton=()=>{
     const {loginWithRedirect} = useAuth0()
     return(
-        <LoginBtn onClick={loginWithRedirect}>Login</LoginBtn>
+    <>
+        <LoginImg src="https://i.ibb.co/McFX1qT/Logo-Frontend-removebg-preview.png" alt="LogoFAdev" />
+        <LoginBtn onClick={loginWithRedirect}>Login with Auth0 - Google</LoginBtn>
+    </>
     )
 }
 
-export const LogoutButton=()=>{
+const LogoutButton=()=>{
     const {logout} = useAuth0()
     return(
         <button onClick={()=>{logout({returnTo:window.location.origin})}}>Logout</button>
@@ -29,10 +43,13 @@ export const Profile=()=>{
     }
     return(
         isAuthenticated &&
-        <div>
-            <img src={user.picture} alt={user.name}/>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-        </div>
+        <>
+            <div>
+                <img src={user.picture} alt={user.name}/>
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
+            <LogoutButton/>
+        </>
     )
 }
