4c42aa7e19e26f3309f60868618e71432e144238
courfedra <franandres93@gmail.com>
2023-01-01 16:56:25 -0300

    set MVP login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import { LoginButton, LogoutButton,Profile } from './components/Login'
+import { useAuth0 } from '@auth0/auth0-react'
 function App() {
-
+  const {isAuthenticated} = useAuth0()
 
   return (
     <>
-      <LoginButton/>
-      <Profile/>
-      <LogoutButton/>
+      {isAuthenticated
+      ?<>
+        <Profile/>
+        <LogoutButton/>
+        </>
+      :<LoginButton/>
+      }
     </>
   )
 }
