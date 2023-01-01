c5c659641acd522deb339442918a23413e58cd3a
courfedra <franandres93@gmail.com>
2023-01-01 16:34:20 -0300

    install auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {auth0Provider} from "@auth0/auth0-react"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <auth0Provider 
+      domain="dev-altmzbyipx5l7eow.us.auth0.com"
+      clientId="vBTvOtMzq6r3zRiBwyZwUTtYOHSbfALR"
+      redirectUri={window.location.origin}>
+      <App />
+    </auth0Provider>
   </React.StrictMode>,
 )
