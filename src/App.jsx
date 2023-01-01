f58a15303660a51a2f22fea6464892a784e69a76
courfedra <franandres93@gmail.com>
2023-01-01 16:40:42 -0300

    create button Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
+import { LoginButton } from './components/Login'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1>Hola</h1>
+    <LoginButton/>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {auth0Provider} from "@auth0/auth0-react"
+import {Auth0Provider} from "@auth0/auth0-react"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <auth0Provider 
+    <Auth0Provider
       domain="dev-altmzbyipx5l7eow.us.auth0.com"
       clientId="vBTvOtMzq6r3zRiBwyZwUTtYOHSbfALR"
-      redirectUri={window.location.origin}>
+      redirectUri={window.location.origin}
+    >
       <App />
-    </auth0Provider>
+    </Auth0Provider>
   </React.StrictMode>,
 )
