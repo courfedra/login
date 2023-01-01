e02b35884e7e862f7745c4062535acefbc68fa2f
courfedra <franandres93@gmail.com>
2023-01-01 17:48:03 -0300

    Adding info to cardLogin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -49,6 +49,7 @@ export const Profile=()=>{
                 <img src={user.picture} alt={user.name}/>
                 <h2>{user.name}</h2>
                 <p>{user.email}</p>
+                <p>{user.repo||"nada"}</p>
             </div>
             <LogoutButton/>
         </>
